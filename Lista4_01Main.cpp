bd48c8319dd609a79195e903c0553c785cf20629
DanielSavio-Hub <69856423+DanielSavio-Hub@users.noreply.github.com>
2020-09-02 14:57:47 -0300

    Validacao do valor de A

---
+++ Lista4_01Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+	class Verify{
+		
+		public: verificaA(int a){
+			if(a<1){
+				printf("a não pode ser menor que 1, por favor tente novamente");
+				return 0;
+			}
+			else{
+				return a;
+			}
+	}
+};
+	
+	int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Portuguese"); //Aceitar caracteres latinos
+	
+	Verify checkA;
+	int a, b, c;
+	
+	printf("Neste prgrama você irá inserir 3 números onde:\nA= um número maior que um\nB= um número qualquer\nC= um número qulquer.");
+	printf("Será mostrado todos os numeros entre B e C divisiveis por A\n ===================================================\n");
+	
+	printf("Digite A: ");
+	scanf("%d", &a);
+	if(checkA.verificaA(a) == 0){
+		return 0;
+	}
+	
+	printf("Digite B: ");
+	scanf("%d", &b);
+	
+	printf("Digite C: ");
+	scanf("%d", &c);	
+}
+
+
+
+
